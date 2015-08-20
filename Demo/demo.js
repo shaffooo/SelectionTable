b3819a608b130f9242df253bcd760d7972bb63ae
Shafiq Ur Rahman <shafiq.tnoli@gmail.com>
2015-08-20 15:12:41 -0500

    updated demo

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,173 +1,274 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  
-    var allUsers = [{id:'1', name:'Monica Anderson'},{id:'2',name:'Steven Blankenship'},{id:'3',name:'Joshua Jones'},{id:'4',name:'Corry Smith'},{id:'5',name:'Vikar Hamilton'},{id:'6',name:'Chandler Bing'},{id:'7',name:'Jessica Woodsmith'},{id:'8',name:'Adams James'},{id: '9', name:'Spencer Deb'}, {id:'10',name:'Brandon Bran'}, {id:'11',name:'Yudi Man'},{id:'12',name:'George Zoto'},{id:'13',name:'Wayman Sorkinvadataker'},{id:'14', name:'Heather Minga'},{id:'15', name:'Sam David'},{id:'16', name:'Ashley Brown'},{id:'17', name:'Tiffany Johns'},{id:'18', name:'Chris Brown'},{id:'19', name:'Ginger White'},{id:'20', name:'Dyan Fowler'},{id:'21', name:'Nancy Smith'},{id:'22', name:'Heather Smith'},{id:'23', name:'Lisa Clark'},{id:'24', name:'Alexis Williams'},{id:'25', name:'Dawn Smith'}, {id:'26', name:'Adam Sorson'}];
+document.addEventListener("DOMContentLoaded", function (event) {
 
-    var editedProjectUsers = [];
+    var allEntities = [{
+        id: '1',
+        name: 'Monica Anderson'
+    }, {
+        id: '2',
+        name: 'Steven Blankenship'
+    }, {
+        id: '3',
+        name: 'Joshua Jones'
+    }, {
+        id: '4',
+        name: 'Corry Smith'
+    }, {
+        id: '5',
+        name: 'Vikar Hamilton'
+    }, {
+        id: '6',
+        name: 'Chandler Bing'
+    }, {
+        id: '7',
+        name: 'Jessica Woodsmith'
+    }, {
+        id: '8',
+        name: 'Adams James'
+    }, {
+        id: '9',
+        name: 'Spencer Deb'
+    }, {
+        id: '10',
+        name: 'Brandon Bran'
+    }, {
+        id: '11',
+        name: 'Yudi Man'
+    }, {
+        id: '12',
+        name: 'George Zoto'
+    }, {
+        id: '13',
+        name: 'Wayman Sorkinvadataker'
+    }, {
+        id: '14',
+        name: 'Heather Minga'
+    }, {
+        id: '15',
+        name: 'Sam David'
+    }, {
+        id: '16',
+        name: 'Ashley Brown'
+    }, {
+        id: '17',
+        name: 'Tiffany Johns'
+    }, {
+        id: '18',
+        name: 'Chris Brown'
+    }, {
+        id: '19',
+        name: 'Ginger White'
+    }, {
+        id: '20',
+        name: 'Dyan Fowler'
+    }, {
+        id: '21',
+        name: 'Nancy Smith'
+    }, {
+        id: '22',
+        name: 'Heather Smith'
+    }, {
+        id: '23',
+        name: 'Lisa Clark'
+    }, {
+        id: '24',
+        name: 'Alexis Williams'
+    }, {
+        id: '25',
+        name: 'Dawn Smith'
+    }, {
+        id: '26',
+        name: 'Adam Sorson'
+    }];
 
-	function SetProjectEditUsers (users, selectedUserIds) {
+    var editedEntities = [];
 
-	    PopulateProjectEditUsers(users);
-	    if (selectedUserIds.length){
-            var actualProjectUsers = users.filter(function (user) {
-                return (selectedUserIds.indexOf(user.id) > -1);
-	        });
-            ChangeElementSelection(actualProjectUsers);
+    function SetProjectEditEntities(entities, selectedEntityIds) {
+
+        PopulateProjectEditEntities(entities);
+
+        // if selected entities exist mark corresponding checkboxex
+        if (selectedEntityIds.length) {
+            var actualProjectEntities = entities.filter(function (entity) {
+                return (selectedEntityIds.indexOf(entity.id) > -1);
+            });
+            ChangeEntitySelection(actualProjectEntities);
         } else {
-            // set selected elements div height and show it.
-            var elementsContentsDiv = document.getElementById("elements-contents");
-            var selectedElementsDiv = document.getElementById("selected-elements");
-            selectedElementsDiv.style.height = ((elementsContentsDiv.clientHeight + 47) + "px");
-            ShowSelectedElements(false);   
+            ShowSelectedEntities(false);
         }
-	}
-    
-	function PopulateProjectEditUsers(users) {
 
-	    var usersTableHTML = "";
+        // set selected entities div height and show it.
+        var entitiesContentsDiv = document.getElementById("entities-contents");
+        var selectedEntitiesDiv = document.getElementById("selected-entities");
+        selectedEntitiesDiv.style.height = ((entitiesContentsDiv.clientHeight + 47) + "px");
+    }
 
-	    //console.log(users.length);
-	    users.forEach(function (user) {
+    function PopulateProjectEditEntities(entities) {
 
-	        usersTableHTML += "<tr>";
+        // clone entities to sort them without effecting incoming array
+        var entitiesClone = entities.slice();
+        entitiesClone = entitiesClone.sort(function (a, b) {
+            if (a.name < b.name) {
+                return -1;
+            }
 
-	        usersTableHTML += "<td style=\"border-right: 1px solid #99CCFF;\">";
-	        usersTableHTML += "<input type=\"checkbox\" id=\"" + user.id + "\" value=\"" + user.name + "\" onclick=\"ChangeSelectedElements(this)\" >";
-	        usersTableHTML += "</td>";
-	        usersTableHTML += "<td>" + user.name + "</td>";
+            if (a.name > b.name) {
+                return 1;
+            }
 
-	        usersTableHTML += "</tr>";
-	    });
-	    document.getElementById("elements").innerHTML = usersTableHTML;
-	}
+            return 0;
+        });
 
-	FilterElements = function (evt) {
+        var entitiesTableHTML = "";
+        entitiesClone.forEach(function (entity) {
 
-	    var filterStr = evt.value.toLowerCase();
-	    var filteredUsers = allUsers.filter(function (user) {
-	        return ((user.name.toLowerCase().indexOf(filterStr)) > -1);
-	    });
+            entitiesTableHTML += "<tr>";
 
-	    PopulateProjectEditUsers(filteredUsers);
-	    ChangeElementSelection(editedProjectUsers);
-	}
+            entitiesTableHTML += "<td style=\"border-right: 1px solid #99CCFF;\">";
+            entitiesTableHTML += "<input type=\"checkbox\" id=\"" + entity.id + "\" value=\"" + entity.name + "\" onclick=\"ChangeSelectedEntities(this)\" >";
+            entitiesTableHTML += "</td>";
+            entitiesTableHTML += "<td>" + entity.name + "</td>";
 
-    GetFilteredElements = function(){
+            entitiesTableHTML += "</tr>";
+        });
+        document.getElementById("entities").innerHTML = entitiesTableHTML;
+    }
 
-        var elementsTable = document.getElementById("elements");
-        var inputs = elementsTable.getElementsByTagName("input");
-        
-        var filteredElements = [];
+    FilterEntities = function (evt) {
+
+        var filterStr = evt.value.toLowerCase();
+        var filteredEntities = allEntities.filter(function (entity) {
+            return ((entity.name.toLowerCase().indexOf(filterStr)) > -1);
+        });
+
+        PopulateProjectEditEntities(filteredEntities);
+        ChangeEntitySelection(editedEntities);
+    }
+
+    GetFilteredEntities = function () {
+
+        var entitiesTable = document.getElementById("entities");
+        var inputs = entitiesTable.getElementsByTagName("input");
+
+        var filteredEntities = [];
         for (var i = 0; i < inputs.length; i++) {
             if (inputs[i].type == "checkbox") {
-                filteredElements.push({id:inputs[i].id, name: inputs[i].value});
+                filteredEntities.push({
+                    id: inputs[i].id,
+                    name: inputs[i].value
+                });
             }
         }
-        
-        return filteredElements;
+
+        return filteredEntities;
     }
-    
-    SelectAll = function (evt) {
 
-        ChangeElementSelection(editedProjectUsers);
-        editedProjectUsers.length = 0;
-	    if (evt.checked) {
-            
-            var filteredElements = GetFilteredElements();
-            ChangeElementSelection(filteredElements);
-	    }   
-	}
+    ChangeAll = function (evt) {
 
-    function ChangeElementSelection(users) {
-        
-        var clonedUsers = users.slice();
-	    clonedUsers.forEach(function (user) {    
-            var cbUser = document.getElementById(user.id);
-	        if (cbUser) {
-	            cbUser.click();
-	        }
-	    });
-	}
-    
-    RemoveSelectedUser = function(evt){
-        
-        var seletedUserDiv = evt.parentNode;
-        if(seletedUserDiv){
-            var selectedUsersDiv = seletedUserDiv.parentNode;
-            if(selectedUsersDiv){
-                selectedUsersDiv.removeChild(seletedUserDiv);
+        ChangeEntitySelection(editedEntities);
+        editedEntities.length = 0;
+        if (evt.checked) {
+
+            var filteredEntities = GetFilteredEntities();
+            ChangeEntitySelection(filteredEntities);
+        }
+    }
+
+    function ChangeEntitySelection(entities) {
+
+        var clonedEntities = entities.slice();
+        clonedEntities.forEach(function (entity) {
+            var cbEntity = document.getElementById(entity.id);
+            if (cbEntity) {
+                cbEntity.click();
             }
-            
-            var selectedUserDivId = seletedUserDiv.id;
-            var selectedUserDivName = seletedUserDiv.innerHTML;
-            var cur_user = { 
-                id: selectedUserDivId.replace('selected', ''), 
-                name: selectedUserDivName.substr(0, selectedUserDivName.indexOf('<span')) 
+        });
+    }
+
+    RemoveSelectedEntity = function (evt) {
+
+        var seletedEntityDiv = evt.parentNode;
+        if (seletedEntityDiv) {
+            var selectedEntitiesDiv = seletedEntityDiv.parentNode;
+            if (selectedEntitiesDiv) {
+                selectedEntitiesDiv.removeChild(seletedEntityDiv);
+            }
+
+            var selectedEntityDivId = seletedEntityDiv.id;
+            var selectedEntityDivName = seletedEntityDiv.innerHTML;
+            var cur_entity = {
+                id: selectedEntityDivId.replace('selected', ''),
+                name: selectedEntityDivName.substr(0, selectedEntityDivName.indexOf('<span'))
             };
-            ChangeElementSelection([cur_user]);
+            ChangeEntitySelection([cur_entity]);
         }
-        
-        var show = ((editedProjectUsers) && (editedProjectUsers.length > 0)) ? true : false;
-	    ShowSelectedElements(show);
+
+        var show = ((editedEntities) && (editedEntities.length > 0)) ? true : false;
+        ShowSelectedEntities(show);
     }
-    
-    ChangeSelectedElements = function(evt) {
-	
-	    var cur_user = { id: evt.id, name: evt.value };
-	    var index = editedProjectUsers.map(function (e) { return e.id; }).indexOf(cur_user.id);
-	    if (evt.checked) {
 
-	        if (index === -1) {
+    ChangeSelectedEntities = function (evt) {
 
-	            editedProjectUsers.push(cur_user);
-	            var editeUsersHTML = document.getElementById('editedusers').innerHTML;
-	            editeUsersHTML += "<div id='" + cur_user.id + "selected' class=\"selected-element\">" + cur_user.name + "<span onclick=\"RemoveSelectedUser(this)\">&#10005</span></div>";
-	            document.getElementById('editedusers').innerHTML = editeUsersHTML;
-	        }
-	    } else {
+        var cur_entity = {
+            id: evt.id,
+            name: evt.value
+        };
+        var index = editedEntities.map(function (e) {
+            return e.id;
+        }).indexOf(cur_entity.id);
+        if (evt.checked) {
 
-	        if (index > -1) {
+            if (index === -1) {
 
-	            editedProjectUsers.splice(index, 1);
-	            // remove div for user by id and updates innerHTML of divinitselectedusers.
-	            var elem = document.getElementById(cur_user.id + 'selected');
-	            if (elem && elem.parentNode) {
-	                elem.parentNode.removeChild(elem);
-	                // = document.getElementById('divinitselectedusers').innerHTML;
-	            }
-	        }
-	    }
-	
-	    var show = ((editedProjectUsers) && (editedProjectUsers.length > 0)) ? true : false;
-	    ShowSelectedElements(show);
-	}
+                editedEntities.push(cur_entity);
+                var editeEntitiesHTML = document.getElementById('selected-entities-contents').innerHTML;
+                editeEntitiesHTML += "<div id='" + cur_entity.id + "selected' class=\"selected-entity\">" + cur_entity.name + "<span onclick=\"RemoveSelectedEntity(this)\">&#10005</span></div>";
+                document.getElementById('selected-entities-contents').innerHTML = editeEntitiesHTML;
+            }
+        } else {
 
-	function ShowSelectedElements(show) {
+            if (index > -1) {
 
-	    if (!show) {
-	        document.getElementById('selected-elements').style.display = "none";
-	    } else {
-	        document.getElementById('selected-elements').style.display = "block";
-	    }
-	}
+                editedEntities.splice(index, 1);
+
+                // remove div for entity by id and updates innerHTML of divinitselectedentities.
+                var elem = document.getElementById(cur_entity.id + 'selected');
+                if (elem && elem.parentNode) {
+                    elem.parentNode.removeChild(elem);
+                }
+            }
+        }
+
+        var show = ((editedEntities) && (editedEntities.length > 0)) ? true : false;
+        ShowSelectedEntities(show);
+    }
+
+    function ShowSelectedEntities(show) {
+
+        if (!show) {
+            document.getElementById('selected-entities').style.display = "none";
+        } else {
+            document.getElementById('selected-entities').style.display = "block";
+        }
+    }
 
 
-	SaveProjectUpdates = function (evt) {
+    SaveProjectUpdates = function (evt) {
 
-	    // issue a post request to save data in db
-        
+        // issue a post request to save data in db
 
 
-	    document.getElementById('selected-elements').innerHTML = '';
-	    editedProjectUsers = [];
-	    registry.byId('dialogProjectEdit').hide();
-	}
 
-	CancelProjectUpdates = function (evt) {
-    
-	    document.getElementById('selected-elements').innerHTML = '';
-	    editedProjectUsers = [];
-	    registry.byId('dialogProjectEdit').hide();
-	}
-  
-    SetProjectEditUsers(allUsers, []);//['1']);
+        document.getElementById('selected-entities').innerHTML = '';
+        editedEntities = [];
+        registry.byId('dialogProjectEdit').hide();
+    }
+
+    CancelProjectUpdates = function (evt) {
+
+        document.getElementById('selected-entities').innerHTML = '';
+        editedEntities = [];
+        registry.byId('dialogProjectEdit').hide();
+    }
+
+    SetProjectEditEntities(allEntities, ['1', '2']);
 });
